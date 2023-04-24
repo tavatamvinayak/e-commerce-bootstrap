f4a22e812641ba99ceccc3395488093b840d34dc
vinayak tavatam <tavatamvinayak@gmail.com>
2023-04-24 23:43:23 +0530

    basic

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,65 @@
 import React from 'react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Navbar = () => {
+const NavBar = () => {
 	return (
 		<>
 			{/* header */}
-			<div>
-				<nav className="navbar navbar-expand-lg ">
+			{/* <div> */}
+
+
+
+
+
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/"> Koza Store </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/" className='active'>Home</Nav.Link>
+            <Nav.Link href="/products" className='active'>newProducts</Nav.Link>
+            
+            <NavDropdown title="Products" id="basic-nav-dropdown" className='active'>
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+			<Nav.Link href="#home" className='active'> <AiOutlineShoppingCart/> </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				{/* <nav className="navbar navbar-expand-lg ">
 					<div className="container-fluid justify-content-between">
 						<a className="navbar-brand" href="#">Navbar</a>
 						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,11 +91,11 @@ const Navbar = () => {
 						</svg></div>
 
 					</div>
-				</nav>
-			</div>
+				</nav> */}
+			{/* </div> */}
 
 		</>
 	);
 }
 
-export default Navbar;
+export default NavBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,91 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Navbar from '@/Components/Navbar'
+import NavBar from '@/Components/Navbar'
 import Footer from '@/Components/Footer'
-import ProductCards from '@/Components/ProductCards'
 import HomeMainImg from '@/Components/HomeMainImg'
 
+
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      <NavBar />
       <main>
         {/* main Images show in Ads */}
         <div>
-          <HomeMainImg/>
+          <HomeMainImg />
         </div>
         {/* Products cards */}
         <div className="album py-5 bg-body-tertiary">
           <div className="container">
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          
-                <ProductCards ProductImage={"https://i.pinimg.com/564x/80/25/c3/8025c3a145cbedc0fe652f012f6754f9.jpg"} ProductName={"niki"} ProductDetails={`fit or good`} ProductPrice={200} />
-                <ProductCards ProductImage={"https://i.pinimg.com/564x/80/25/c3/8025c3a145cbedc0fe652f012f6754f9.jpg"} ProductName={"niki"} ProductDetails={`fit or good`} ProductPrice={200} />
-                <ProductCards ProductImage={"https://i.pinimg.com/564x/80/25/c3/8025c3a145cbedc0fe652f012f6754f9.jpg"} ProductName={"niki"} ProductDetails={`fit or good`} ProductPrice={200} />
-                <ProductCards ProductImage={"https://i.pinimg.com/564x/80/25/c3/8025c3a145cbedc0fe652f012f6754f9.jpg"} ProductName={"niki"} ProductDetails={`fit or good`} ProductPrice={200} />
-                <ProductCards ProductImage={"https://i.pinimg.com/564x/80/25/c3/8025c3a145cbedc0fe652f012f6754f9.jpg"} ProductName={"niki"} ProductDetails={`fit or good`} ProductPrice={200} />
-         
+
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://rukminim1.flixcart.com/image/832/832/xif0q/top/u/o/7/m-003-collest-original-imagzufjd4wmruzz.jpeg?q=70" className='h-5' />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
 
             </div>
           </div>
